59c1bd5178b6beaf35dc3001f7e9abda2cba62bc
A. Unique TensorFlower <gardener@tensorflow.org>
2024-06-03 11:22:53 -0700

    Export /tensorflow/serving/batching/configured_batch_padding_policy metric for SharedBatchScheduler.
    Reverts efc89fa94b37d378faaffb31ebc127c8ac42dcb7
    
    PiperOrigin-RevId: 639852695

--- tensorflow/core/kernels/batching_util/batch_resource_base.h
+++ tensorflow/core/kernels/batching_util/batch_resource_base.h
@@ -34,6 +34,7 @@ limitations under the License.
 #include "tensorflow/core/framework/tensor_shape.h"
 #include "tensorflow/core/kernels/batching_util/adaptive_shared_batch_scheduler.h"
 #include "tensorflow/core/kernels/batching_util/batch_scheduler.h"
+#include "tensorflow/core/kernels/batching_util/batch_scheduler_utils.h"
 #include "tensorflow/core/kernels/batching_util/shared_batch_scheduler.h"
 #include "tensorflow/core/kernels/batching_util/threadsafe_status.h"
 #include "tensorflow/core/platform/context.h"
@@ -52,6 +53,7 @@ struct BatchResourceOptions {
   int32_t batch_timeout_micros;
   int32_t max_enqueued_batches;
   std::vector<int32_t> allowed_batch_sizes;
+  std::string batch_padding_policy{kPadUpPolicy};
   int32_t low_priority_max_batch_size;
   int32_t low_priority_batch_timeout_micros;
   int32_t low_priority_max_enqueued_batches;
@@ -213,6 +215,7 @@ class BatchResourceBase : public ResourceBase {
       int32_t batch_timeout_micros, int32_t max_enqueued_batches,
       const std::vector<int32>& allowed_batch_sizes,
       bool enable_large_batch_splitting, bool disable_padding,
+      const std::string& batch_padding_policy,
       int32_t low_priority_max_batch_size,
       int32_t low_priority_batch_timeout_micros,
       int32_t low_priority_max_enqueued_batches,
@@ -332,9 +335,14 @@ class BatchResourceBase : public ResourceBase {
   static Status EmitIndexTensor(OpKernelContext* context, const BatchT& batch,
                                 int output_index);
 
-  // Looks up the batcher queue for 'queue_name'. If it did't previously exist,
+  // Looks up the batcher queue for 'queue_name'. If it didn't previously exist,
   // creates it.
+  //
+  // The model_name and op_name are the names of the current model and
+  // operation, respectively.
   Status LookupOrCreateBatcherQueue(const string& queue_name,
+                                    const string& model_name,
+                                    const string& op_name,
                                     BatcherQueueT** queue);
 
   SessionMetadata session_metadata_;
